75131e2a441100231dfd76a0b9d4aa70d174d2a4
PJ Khamvongsa <pjkhamvongsa@gmail.com>
2021-03-22 12:52:45 -0400

    changing styles

--- tictactoe/src/components/Square.js
+++ tictactoe/src/components/Square.js
@@ -3,10 +3,8 @@ import React from "react";
 const Square = ({value, onClick}) => {
     const style = value ? `squares ${value}` :  `squares`;
 
-
     return(
-        <button className={style} onClick={onClick}>
-            {value}
+        <button className={style} onClick={onClick}>{value}
         </button>
     )
 }
